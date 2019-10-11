9281f023b4c6611a141cb8495c7dcdfb2decb6e1
Symeonw <Symeonwhite16@gmail.com>
2019-10-11 11:22:18 -0500

    update

--- regression/split_scale.py
+++ regression/split_scale.py
@@ -4,7 +4,7 @@ warnings.filterwarnings("ignore")
 import numpy as numpy
 import wrangle
 import env
-from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+
 
 df = wrangle_telco()
 train, test = train_test_split(df, train_size = .7, random_state = 123)
@@ -16,7 +16,15 @@ def split_my_data(df,t_size,seed,split_on):
     y = df[split_on]
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = t_size, random_state = seed)
     return x_train, x_test, y_train, y_test
+
+
 split_my_data(df,.7,123,"tenure")
 
 
 def standard_scaler():
+    train = x_train,y_train
+    from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+    scaler = StandardScaler()
+    output = scaler.fit(train):scaler.transform(train),scaler.transform(test)
+
+standard_scaler()
