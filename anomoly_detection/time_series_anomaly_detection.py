81175ee7dc45f84f6d624f72d76f5eb1801e5159
Symeon White <54292200+Symeonw@users.noreply.github.com>
2020-01-30 19:51:29 -0600

    Delete time_series_anomaly_detection.py

--- anomoly_detection/time_series_anomaly_detection.py
+++
@@ -1,7 +0,0 @@
-#Time series + EMA
-#Discover users who are accessing our
-#  curriculum pages way beyond the end 
-# of their codeup time. What would the 
-# dataframe look like? Use time series 
-# method for detecting anomalies, like 
-# exponential moving average with %b.
